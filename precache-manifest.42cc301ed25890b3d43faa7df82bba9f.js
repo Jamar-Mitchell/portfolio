ddc25463fb5dea091580b397ed5cfe6cf64066ef
Jamar-Mitchell <jamar-mitchell@hotmail.com>
2019-10-09 01:11:18 -0400

    Updates

---
+++ precache-manifest.42cc301ed25890b3d43faa7df82bba9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe79a9c4c5a9237f549bcfc42c46700",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8accabf81a3a5f734633",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "82f1183d1cea7954a5fa",
+    "url": "/portfolio/static/js/2.75eb472c.chunk.js"
+  },
+  {
+    "revision": "8accabf81a3a5f734633",
+    "url": "/portfolio/static/js/main.c53a04d1.chunk.js"
+  },
+  {
+    "revision": "32f9199c3476d1e69b39",
+    "url": "/portfolio/static/js/runtime-main.bceb1215.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
